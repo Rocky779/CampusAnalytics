4a065a3e6a74fe0f593b089880847fefa149b286
rishishukla <rsa27777@gmail.com>
2024-02-13 19:51:48 -0800

    Fixed cache progress

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -24,24 +24,29 @@ export default class InsightFacade implements IInsightFacade {
 	constructor() {
 		this.queryHelper = new QueryHelper();
 		// LOAD PREVIOUS SAVED DATASET HERE
-		this.loadDatasetIds().then(() => {
+		this.init().then(() => {
 			console.log("Dataset IDs loaded successfully:", this.datasetIds);
 		}).catch((error) => {
-			console.error("Error loading dataset IDs:", error);
+			console.log(error);
 		});
 		console.log("InsightFacadeImpl::init()");
 	}
 
-	private async loadDatasetIds() {
+	public async init() {
+		await this.loadDatasetIds();
+	}
+
+	public async loadDatasetIds() {
 		const dataFolderPath = "data"; // Path to the data folder
 		try {
 			// Read the contents of the data folder
 			const files = await fs.readdir(dataFolderPath);
-
 			// Extract dataset IDs from file names
 			files.forEach((file) => {
 				const id = file.replace(".json", ""); // Remove the file extension
-				this.datasetIds.push(id);
+				if (!this.datasetIds.includes(id)) {
+					this.datasetIds.push(id);
+				}
 			});
 
 			console.log("Loaded dataset IDs:", this.datasetIds);
@@ -59,6 +64,7 @@ export default class InsightFacade implements IInsightFacade {
 	 * @returns A promise that resolves to an array of the current dataset IDs upon success.
 	 */
 	public async addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
+		await this.loadDatasetIds();
 		if (this.isEntryInValid(id, content, kind)) {
 			return Promise.reject(new InsightError("ERROR"));
 		}
@@ -236,12 +242,15 @@ export default class InsightFacade implements IInsightFacade {
 	}
 
 	public async removeDataset(id: string): Promise<string> {
+		await this.loadDatasetIds();
 		// check if ID is valid
 		if (this.isInvalidID(id)) {
 			return Promise.reject(new InsightError("Invalid ID"));
 		}
 		// check if ID already exists
 		if (!this.datasetIds.includes(id)) {
+			console.log("Debug");
+			console.log(this.datasetIds);
 			return Promise.reject(new NotFoundError("This ID does not exist in the datasetIDs"));
 		}
 		// Actually remove the data
@@ -256,6 +265,7 @@ export default class InsightFacade implements IInsightFacade {
 	}
 
 	public async listDatasets(): Promise<InsightDataset[]> {
+		await this.loadDatasetIds();
 		try {
 			if (this.datasetIds.length === 0) {
 				return Promise.resolve([]); // Return empty array if no dataset IDs are available
